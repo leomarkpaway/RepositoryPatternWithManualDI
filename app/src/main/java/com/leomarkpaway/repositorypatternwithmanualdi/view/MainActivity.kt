0d2baa7596f74ff8649a06b20cff4a5adb157319
leomarkpaway <leomarkpaway@gmail.com>
2024-07-03 18:56:08 +0800

    :sparkles: Create save note function

--- app/src/main/java/com/leomarkpaway/repositorypatternwithmanualdi/view/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/repositorypatternwithmanualdi/view/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.leomarkpaway.repositorypatternwithmanualdi.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.leomarkpaway.repositorypatternwithmanualdi.MyApp
 import com.leomarkpaway.repositorypatternwithmanualdi.R
 import com.leomarkpaway.repositorypatternwithmanualdi.viewmodel.MainViewModel
@@ -10,6 +15,9 @@ import com.leomarkpaway.repositorypatternwithmanualdi.viewmodel.MainViewModelFac
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
+    private lateinit var editNote: EditText
+    private lateinit var saveButton: Button
+    private lateinit var noteList: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +26,36 @@ class MainActivity : AppCompatActivity() {
         val factory = MainViewModelFactory(MyApp.appModule.noteRepository)
         viewModel = ViewModelProvider(viewModelStore, factory)[MainViewModel::class.java]
 
+        editNote = findViewById(R.id.edt_note)
+        saveButton = findViewById(R.id.btn_save_note)
+        noteList = findViewById(R.id.rv_note_list)
+
+        onSaveNote()
+        setupNoteList()
+
     }
-    
+
+    private fun onSaveNote() {
+        var note: String? = null
+
+        editNote.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                note = s.toString()
+            }
+        })
+
+        saveButton.setOnClickListener {
+            if (!note.isNullOrEmpty()) {
+                viewModel.saveNote(note!!)
+            }
+        }
+
+    }
+
+    private fun setupNoteList() {
+        TODO("Not yet implemented")
+    }
+
 }
--- app/src/main/java/com/leomarkpaway/repositorypatternwithmanualdi/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/leomarkpaway/repositorypatternwithmanualdi/viewmodel/MainViewModel.kt
@@ -1,6 +1,19 @@
 package com.leomarkpaway.repositorypatternwithmanualdi.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.leomarkpaway.repositorypatternwithmanualdi.data.repository.NoteRepository
+import com.leomarkpaway.repositorypatternwithmanualdi.data.source.local.entity.Note
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainViewModel(private val repository: NoteRepository) : ViewModel() {}
+class MainViewModel(private val noteRepository: NoteRepository) : ViewModel() {
+
+    fun saveNote(noteString: String) {
+       viewModelScope.launch(Dispatchers.IO) {
+           val noteItem = Note(null, noteString)
+           noteRepository.saveNote(noteItem)
+       }
+    }
+
+}
