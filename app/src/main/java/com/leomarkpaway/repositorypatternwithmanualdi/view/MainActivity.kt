2f1bb722ab01e60866fa5c5a5ba2884cfcc78bb3
leomarkpaway <leomarkpaway@gmail.com>
2024-07-03 18:05:42 +0800

    :sparkles: Create global viewmodel with note repository

--- app/src/main/java/com/leomarkpaway/repositorypatternwithmanualdi/view/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/repositorypatternwithmanualdi/view/MainActivity.kt
@@ -2,13 +2,22 @@ package com.leomarkpaway.repositorypatternwithmanualdi.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.leomarkpaway.repositorypatternwithmanualdi.MyApp
 import com.leomarkpaway.repositorypatternwithmanualdi.R
+import com.leomarkpaway.repositorypatternwithmanualdi.viewmodel.MainViewModel
+import com.leomarkpaway.repositorypatternwithmanualdi.viewmodel.MainViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val factory = MainViewModelFactory(MyApp.appModule.noteRepository)
+        viewModel = ViewModelProvider(viewModelStore, factory)[MainViewModel::class.java]
+
     }
     
 }
