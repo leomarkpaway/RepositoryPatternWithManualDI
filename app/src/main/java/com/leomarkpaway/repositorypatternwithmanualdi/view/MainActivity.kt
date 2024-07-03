c70e7bc85dd6024c054c10bb1a59840109430552
leomarkpaway <leomarkpaway@gmail.com>
2024-07-03 18:01:27 +0800

    :hammer: Move main activity inside view package

--- app/src/main/java/com/leomarkpaway/repositorypatternwithmanualdi/view/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/repositorypatternwithmanualdi/view/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.leomarkpaway.repositorypatternwithmanualdi
+package com.leomarkpaway.repositorypatternwithmanualdi.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.leomarkpaway.repositorypatternwithmanualdi.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    
 }
