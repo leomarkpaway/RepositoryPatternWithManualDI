44886ac598eeef43e64f54baaf46c4a4f15feab8
leomarkpaway <leomarkpaway@gmail.com>
2024-07-03 19:40:05 +0800

    :sparkles: Add get all notes in repository

--- app/src/main/java/com/leomarkpaway/repositorypatternwithmanualdi/data/repository/NoteRepository.kt
+++ app/src/main/java/com/leomarkpaway/repositorypatternwithmanualdi/data/repository/NoteRepository.kt
@@ -1,14 +1,17 @@
 package com.leomarkpaway.repositorypatternwithmanualdi.data.repository
 
+import androidx.lifecycle.LiveData
 import com.leomarkpaway.repositorypatternwithmanualdi.data.source.local.doa.NoteDao
 import com.leomarkpaway.repositorypatternwithmanualdi.data.source.local.entity.Note
 
 interface NoteRepository {
    suspend fun saveNote(note: Note)
+   suspend fun getAllNote() : LiveData<List<Note>>
 }
 
 class NoteRepositoryImpl(private val todoDao: NoteDao) : NoteRepository {
 
     override suspend fun saveNote(note: Note) = todoDao.saveNote(note)
+    override suspend fun getAllNote() = todoDao.getAllNote()
 
 }
--- app/src/main/java/com/leomarkpaway/repositorypatternwithmanualdi/data/source/local/doa/NoteDao.kt
+++ app/src/main/java/com/leomarkpaway/repositorypatternwithmanualdi/data/source/local/doa/NoteDao.kt
@@ -1,8 +1,10 @@
 package com.leomarkpaway.repositorypatternwithmanualdi.data.source.local.doa
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.leomarkpaway.repositorypatternwithmanualdi.data.source.local.entity.Note
 
 @Dao
@@ -10,5 +12,8 @@ interface NoteDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun saveNote(note: Note)
+
+    @Query("SELECT * FROM notes ORDER BY title ASC")
+    fun getAllNote(): LiveData<List<Note>>
 
 }
